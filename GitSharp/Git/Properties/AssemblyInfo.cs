317547b75ff3821c4e2c2239d655f16979dbcb31
Tiernan OToole <tiernan@tiernanotoole.ie>
2011-07-05 09:34:29 +0100

    removing GitSharp Code

--- GitSharp/Git/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("git")]
-[assembly: AssemblyDescription("Command line interface for GitSharp")]
--- GitSharp/GitSharp.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("GitSharp.Tests")]
-[assembly: AssemblyDescription("")]
